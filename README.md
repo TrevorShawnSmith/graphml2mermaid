196f7e033596732ed472fd38369e633ec285535f
= <=>
2025-06-18 11:13:43 +0900

    Sanitized strings for better graph conversion

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # graphml2mermaid
 
 Simple script to convert graphml files (yEd) to mermaid syntax.
+
+Capture the output via bash as 
+~~~ 
+python graphml2mermaid [filename] > tmp.mmd
+~~~
--- graphml2mermaid.py
+++ graphml2mermaid.py
@@ -1,31 +1,32 @@
+
+import html
 import click
+import networkx as nx
 from networkx.readwrite.graphml import read_graphml
 
+def escape_label(label):
+    # Replace newlines with <br> and escape HTML special chars
+    return html.escape(label).replace('\n', '<br>')
 
 @click.command()
 @click.argument('filename')
 def convert(filename):
-
-    # TODO: more options, read edge type, node shape etc.
-
     result = ['graph TB']
-
     G = read_graphml(filename)
 
     for node, label in G.nodes.data('label'):
         if label:
-            result.append(f'    {node}["{label}]"')
+            safe_label = escape_label(str(label))
+            result.append(f'    {node}["{safe_label}"]')
 
-    edge_labels = {
-        (source, target): f'- {label} -' if label else ''
-        for source, target, label in G.edges.data('label')
-    }
-    for source, target in G.edges():
-        label = edge_labels[(source, target)]
-        result.append(f'    {source}-"{label}"->{target}')
+    for source, target, data in G.edges(data=True):
+        label = escape_label(str(data.get('label', '')))
+        if label:
+            result.append(f'    {source} -- {label} --> {target}')
+        else:
+            result.append(f'    {source} --> {target}')
 
     print('\n'.join(result))
-
 
 if __name__ == '__main__':
     convert()
